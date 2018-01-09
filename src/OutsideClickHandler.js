8cfafb256d70d62821e8d008d85ee37235047c09
YozhikM <botev.st@gmail.com>
2018-01-09 20:07:37 +0600

    feat: add component, tests

---
+++ src/OutsideClickHandler.js
@@ -0,0 +1,46 @@
+/* @flow */
+
+import * as React from 'react';
+
+type Props = {
+  children: React.Node,
+  onOutsideClick: Function,
+};
+
+export default class OutsideClickHandler extends React.Component<Props, void> {
+  childNode: ?HTMLElement;
+  onOutsideClick: Function;
+  setChildNodeRef: Function;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.onOutsideClick = this.onOutsideClick.bind(this);
+    this.setChildNodeRef = this.setChildNodeRef.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.onOutsideClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onOutsideClick);
+  }
+
+  onOutsideClick(e: any) {
+    const { onOutsideClick } = this.props;
+    const { childNode } = this;
+    const isDescendantOfRoot = childNode && childNode.contains(e.target);
+    if (!isDescendantOfRoot) {
+      onOutsideClick();
+    }
+  }
+
+  setChildNodeRef(ref: ?HTMLElement) {
+    this.childNode = ref;
+  }
+
+  render() {
+    return <div ref={this.setChildNodeRef}>{this.props.children}</div>;
+  }
+}
---
+++ src/__tests__/OutsideClickHandler-test.js
@@ -0,0 +1,74 @@
+/* @flow */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import jest, { describe, it, expect } from 'jest';
+import OutsideClickHandler from '../OutsideClickHandler';
+
+const ChildrenComponent = () => <span>I'm children!</span>;
+
+describe('OutsideClickHandler', () => {
+  it('render()', () => {
+    const onOutsideClick = jest.fn();
+    const wrapper = shallow(
+      <OutsideClickHandler onOutsideClick={onOutsideClick}>
+        <ChildrenComponent />
+      </OutsideClickHandler>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('componentDidMount()', () => {
+    const onOutsideClick = jest.fn();
+    const wrapper = shallow(
+      <OutsideClickHandler onOutsideClick={onOutsideClick}>
+        <ChildrenComponent />
+      </OutsideClickHandler>
+    );
+    // $FlowFixMe
+    document.addEventListener = jest.fn();
+    wrapper.instance().componentDidMount();
+
+    expect(document.addEventListener).toBeCalledWith('click', wrapper.instance().onOutsideClick);
+  });
+
+  it('componentWillUnmount()', () => {
+    const onOutsideClick = jest.fn();
+    const wrapper = shallow(
+      <OutsideClickHandler onOutsideClick={onOutsideClick}>
+        <ChildrenComponent />
+      </OutsideClickHandler>
+    );
+    // $FlowFixMe
+    document.removeEventListener = jest.fn();
+    wrapper.instance().componentWillUnmount();
+
+    expect(document.removeEventListener).toBeCalledWith('click', wrapper.instance().onOutsideClick);
+  });
+
+  it('onOutsideClick()', () => {
+    const e = jest.fn();
+    const onOutsideClick = jest.fn();
+    const wrapper = shallow(
+      <OutsideClickHandler onOutsideClick={onOutsideClick}>
+        <ChildrenComponent />
+      </OutsideClickHandler>
+    );
+    wrapper.instance().onOutsideClick(e);
+
+    expect(onOutsideClick).toBeCalled();
+  });
+
+  it('setChildNodeRef()', () => {
+    const onOutsideClick = jest.fn();
+    const wrapper = shallow(
+      <OutsideClickHandler onOutsideClick={onOutsideClick}>
+        <ChildrenComponent />
+      </OutsideClickHandler>
+    );
+    wrapper.instance().setChildNodeRef(<ChildrenComponent />);
+
+    expect(wrapper.instance().childNode).toEqual(<ChildrenComponent />);
+  });
+});
